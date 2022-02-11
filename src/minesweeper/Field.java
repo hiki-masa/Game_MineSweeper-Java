ae433b29eaeff0b077494f43cf916170fa1ce6b0
hiki-masa <hikimasa.micro@gmail.com>
2022-02-11 12:09:52 +0900

    add Window.java

--- src/minesweeper/Field.java
+++ src/minesweeper/Field.java
@@ -4,29 +4,52 @@ import java.util.ArrayList;
 
 public class Field {
 	int FieldSize;
-	ArrayList<ArrayList<Integer>> Field = new ArrayList<ArrayList<Integer>>();
+	ArrayList<ArrayList<Mine>> Field = new ArrayList<ArrayList<Mine>>();
+	Window frame;
 	
 	// コンストラクタ
 	public Field(int _FieldSize) {
 		this.FieldSize = _FieldSize;
-		
+		//frame = new Window("MineSweeper");
 		for (int i = 0; i < this.FieldSize; i++) {
-			ArrayList<Integer> tmp = new ArrayList<>();
+			ArrayList<Mine> tmp = new ArrayList<>();
+			Mine tmp_mine = new Mine();
 			for (int j = 0; j < this.FieldSize; j++) {
-				tmp.add(j);
+				tmp.add(tmp_mine);
 			}
 			this.Field.add(tmp);
 		}
-		//System.out.println(this.Field);
 	}
 	
 	// マップ全体の表示
 	public void disp() {
-		for (int x =0; x < this.FieldSize; x++) {
-			for (int y : this.Field.get(x)) {
-				System.out.print(y);
+		for (int x = 0; x < this.FieldSize; x++) {
+			ArrayList<Mine> tmp_Field = this.Field.get(x);
+			for (int y = 0; y < this.FieldSize; y++) {
+				if (tmp_Field.get(y).open) {
+					System.out.print("□");
+				}
+				else {
+					System.out.print("■");
+				}
 			}
 			System.out.println();
 		}
+	}
+}
+
+class Mine {
+	// マスが開けられたかの判定
+	Boolean open;
+	// 地雷の有無
+	Boolean mine;
+	// チェックの有無
+	Boolean check;
+	
+	// コンストラクタ
+	public Mine() {
+		open = false;
+		mine = false;
+		check = false;
 	}
 }
